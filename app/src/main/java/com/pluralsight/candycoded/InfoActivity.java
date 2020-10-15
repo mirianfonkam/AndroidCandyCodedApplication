6f0595dec32c029eb437ca59ca76e0eca7801946
Mirian Fonkam <mlcf98@gmail.com>
2020-10-15 17:41:50 -0300

    2nd  commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,7 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
+    // Task 2 - Launch the Google Maps Activity
     // ***
 
     public void createMapIntent(View view) {
